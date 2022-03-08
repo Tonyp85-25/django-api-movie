5659404124beeae4ff5a9ed412ffa59064e419c6
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2022-03-08 23:15:27 +0100

    actors app setup

---
+++ app/actors/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Actor
+from .serializers import ActorSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
